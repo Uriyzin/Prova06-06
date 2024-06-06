a0ff4169cff50688708ebfeae40c04e8de7c0203
Uriyzin <166513070+Uriyzin@users.noreply.github.com>
2024-06-06 09:07:13 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,84 @@
+//Exercício 2
+let = nome = document.getElementById("nome")
+let resposta = document.getElementById("resposta")
+
+function Clicar()
+{
+    resposta.innerHTML ="Olá, " + nome.value + ", tudo bem?"
+}
+
+//Exercício 3
+let = centimetros = document.getElementById("centimetros")
+let metros = document.getElementById("metros")
+
+function Converter()
+{
+    metros.innerHTML = centimetros.value / 100 + " m"
+}
+
+//Exercício 4
+let numero = document.getElementById("inteiro")
+let antecessor = document.getElementById("antecessor")
+let sucessor = document.getElementById("sucessor")
+
+function VerRespostas()
+{
+    antecessor.innerHTML = parseFloat(numero.value) - 1
+    sucessor.innerHTML = parseFloat(numero.value) + 1
+}
+
+//Exercício 5
+let n1 = document.getElementById("n1")
+let n2 = document.getElementById("n2")
+let descobrir = document.getElementById("descobrir")
+
+function lerNumeros()
+{
+    if(n1.value > n2.value)
+        {
+            descobrir.innerHTML = n1.value + " é maior"
+        }
+    else
+        {
+            descobrir.innerHTML = n2.value + " é maior"
+        }
+}
+
+//Exercício 6
+let salario = document.getElementById("salario");
+let aumento = document.getElementById("aumento")
+
+function total()
+{
+    if(salario.value <= 1250)
+        {
+            aumento.innerHTML = "Aumento de $" + parseFloat(salario.value * 15) / 100 + ",00.";
+        }
+        else
+        {
+            aumento.innerHTML = "Aumento de $" + parseFloat(salario.value * 10) / 100 + ",00."
+        }
+}
+
+//Exercício 7
+console.log("exercício 7")
+a = 1
+while (a <= 100){
+        console.log(a)
+        a++
+}
+
+//Execício 8
+console.log("exercicio 8")
+b = 0
+while (b <= 50){
+        console.log(b)
+        b = b + 5
+}
+//Exercício 9
+console.log("exercício 9")
+c = 100
+while (c >= 0){
+        console.log(c)
+        c = c - 10
+}
